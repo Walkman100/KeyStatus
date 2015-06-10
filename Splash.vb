9b7edf1c51fa1236e65770a1af1b2bdb84ca45a8
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2015-06-10 20:25:13 +0200

    Removed unneccesary forms, renamed Form1.* to KeyStatus.*

--- Splash.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class Splash
-
-    Private Sub Splash_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        LabelX1.ForeColor = Color.Gray
-        LabelX1.Text = My.Application.Info.ProductName
-    End Sub
-End Class
